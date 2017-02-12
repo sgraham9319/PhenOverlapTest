0d11a0f5ff1ee11da0c4897f1187806c82f123f0
sgraham9319 <sg9319@my.bristol.ac.uk>
2017-02-12 15:18:34 -0800

    Calculates overlap for all species combinations in all years at one site

--- PhenOverlap_SG.R
+++ PhenOverlap_SG.R
@@ -24,71 +24,68 @@ names(x1)
 chaut <- droplevels(chaut[chaut$Species %in% names(x1),])
 table(chaut$Species)
 
-#############################
-# First try doing just 1 year
-#############################
+############################################################
+# Calculating phenological overlap for all years within site
+############################################################
 
-# Subset to 1 year
-chaut <- chaut[chaut$year == 2008, ]
-
-########################
-# Combinations both ways
-########################
+# Create vector of years included in sampling
+years <- unique(chaut$year)
 
 # Create vector of species names in alphabetical order
 sps <- sort(unique(chaut$Species))
 
-# Create a matrix with a cell for each species combination
-results <- matrix(nrow = length(sps), ncol = length(sps))
+# Create an array with a cell for each species combination in each year
+results <- array(dim = c(length(sps), length(sps), length(years)), 
+                 dimnames = list(sps, sps, years))
 
-# Attach species names to the columns and rows
-colnames(results) <- sps
-rownames(results) <- sps
 
-#################################
-# Evaluating phenological overlap
-#################################
-
-# Loop through the focal species (sp1)
-for(sp1 in 1:length(sps)) {
-  # Find ordinal dates when focal species present
-  dates <- chaut[chaut$Species == sps[sp1] & chaut$Stage6 > 0, "OrdinalDate"]
-  # Subset to these dates
-  sub.dat <- chaut[chaut$OrdinalDate %in% dates,]
-  # Calculate total number adults of focal species in year
-  tot.sp1 <- tapply(sub.dat$Stage6, sub.dat$Species, FUN = sum)[sps[sp1]]
-  # Create empty vector for overlap by ordinal date
-  measures <- rep(NA, times = length(dates))
+for(year in 1:length(years)) {
+  # Subset to 1 year
+  sub.year <- chaut[chaut$year == years[year], ]
   
-  # Loop through each competitor species (sp2)
-  for(sp2 in 1:length(sps)) {
-    # Avoid calculating overlap between a focal species and itself
-    if(sp2 != sp1) {
-      # Subset to the 2 species of interest
-      sub.sp <- sub.dat[sub.dat$Species == sps[sp1] | 
-                          sub.dat$Species == sps[sp2], ]
-      
-      # Loop through dates, calculating overlap for each date
-      for (ord in 1:length(dates)) {
-        # Extract number of adults for focal species
-        focals <- sub.sp[sub.sp$OrdinalDate == dates[ord] & 
-                        sub.sp$Species == sps[sp1], "Stage6"]
-        # Extract number of adults for competitor species
-        competitors <- sub.sp[sub.sp$OrdinalDate == dates[ord] & 
-                             sub.sp$Species == sps[sp2], "Stage6"]
-        # Check that a row representing competitor species on this date exists
-        if(length(competitors) == 0){
-          # If no row exists make number of competitors zero
-          competitors <- 0
+  # Loop through the focal species (sp1)
+  for(sp1 in 1:length(sps)) {
+    # Find ordinal dates when focal species present
+    dates <- sub.year[sub.year$Species == sps[sp1] & sub.year$Stage6 > 0, 
+                      "OrdinalDate"]
+    # Subset to these dates
+    sub.dat <- sub.year[sub.year$OrdinalDate %in% dates,]
+    # Calculate total number adults of focal species in year
+    tot.sp1 <- tapply(sub.dat$Stage6, sub.dat$Species, FUN = sum)[sps[sp1]]
+    # Create empty vector for overlap by ordinal date
+    measures <- rep(NA, times = length(dates))
+    
+    # Loop through each competitor species (sp2)
+    for(sp2 in 1:length(sps)) {
+      # Avoid calculating overlap between a focal species and itself
+      if(sp2 != sp1) {
+        # Subset to the 2 species of interest
+        sub.sp <- sub.dat[sub.dat$Species == sps[sp1] | 
+                            sub.dat$Species == sps[sp2], ]
+        
+        # Loop through dates, calculating overlap for each date
+        for (ord in 1:length(dates)) {
+          # Extract number of adults for focal species
+          focals <- sub.sp[sub.sp$OrdinalDate == dates[ord] & 
+                             sub.sp$Species == sps[sp1], "Stage6"]
+          # Extract number of adults for competitor species
+          competitors <- sub.sp[sub.sp$OrdinalDate == dates[ord] & 
+                                  sub.sp$Species == sps[sp2], "Stage6"]
+          # Check that a row representing competitor species on this date exists
+          if(length(competitors) == 0){
+            # If no row exists make number of competitors zero
+            competitors <- 0
+          }
+          # Calculate ratio of competitors to focals
+          ratio <- competitors/focals
+          # Weight the ratio by the proportion of focal adults for the year that
+          # were present on this date
+          measures[ord] <- ratio*(focals/tot.sp1)
         }
-        # Calculate ratio of competitors to focals
-        ratio <- competitors/focals
-        # Weight the ratio by the proportion of focal adults for the year that
-        # were present on this date
-        measures[ord] <- ratio*(focals/tot.sp1)
+        # Sum measures of overlap to obtain weighted mean and add to results matrix
+        results[sp1, sp2, year] <- sum(measures)
       }
-      # Sum measures of overlap to obtain weighted mean and add to results matrix
-      results[sp1, sp2] <- sum(measures)
     }
   }
 }
+
